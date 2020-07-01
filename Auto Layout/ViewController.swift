1168acb3455a9e615bccf1299536ef832a2b1aaa
Luiz <luizhsou1@gmail.com>
2020-07-01 13:42:00 -0300

    elememtos basicos do auto layout

--- Auto Layout/ViewController.swift
+++ Auto Layout/ViewController.swift
@@ -17,16 +17,25 @@ class ViewController: UIViewController {
         view.addSubview(redView)
         
         redView.backgroundColor = .red
+        redView.translatesAutoresizingMaskIntoConstraints = false // Para poder setar contraints de forma manual
         
-        // Para poder setar contraints de forma manual
-        redView.translatesAutoresizingMaskIntoConstraints = false
-        redView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
-        redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
-        redView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
-        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+        /***
+            Observação: Uma contraint pode interferir na outra, por isso algumas estarão comentadas
+         */
         
+        // CONTRAINTS BÁSICAS
+//        redView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+//        redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
+//        redView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
+//        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
         
+        // CONTRAINTS DE LARGURA E ALTURA
+        redView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        redView.heightAnchor.constraint(equalToConstant: 300).isActive = true
         
+        // CONTRAINTS DE ALINHAMENTO VERTICAL E HORIZONTAL
+        redView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        redView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 
 
