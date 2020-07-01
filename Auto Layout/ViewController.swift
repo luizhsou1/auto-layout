b0e8109748f0a337d6508e2dcc71ba615b613877
Luiz <luizhsou1@gmail.com>
2020-07-01 13:24:45 -0300

    Initial Commit

---
+++ Auto Layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout
+//
+//  Created by Luiz on 01/07/20.
+//  Copyright © 2020 Zennit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
